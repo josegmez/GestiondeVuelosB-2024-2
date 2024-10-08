d20b1e704b08f4346b7115595f8c29120dafda69
Jose David Gómez <jose.gomez14@udea.edu.co>
2024-10-08 20:45:05 +0000

    build: :construction_worker: Update structure and env vars to deploy app on railway

--- BackREST/src/main/java/com/udea/vueloudea/repository/IAirplaneRepository.java
+++
@@ -1,9 +0,0 @@
-package com.udea.vueloudea.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.udea.vueloudea.model.AirplaneType;
-
-@Repository
-public interface IAirplaneRepository extends JpaRepository<AirplaneType, Long> {
-}
